e9f8dba37b7e7d81b9532019a0ad1a63582cb4a2
AlexanderJCS <alexandercastronovo@gmail.com>
2023-11-30 10:21:38 -0500

    switch to using pandas

--- src/exospotter/grouping.py
+++ src/exospotter/grouping.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import pandas as pd
 
 
 @dataclass(frozen=True)
@@ -20,8 +21,8 @@ class Group:
     start_time: float
     end_time: float
     mean_flux: float
-    peak_flux_dp: Datapoint
-    min_flux_dp: Datapoint
+    peak_flux: float
+    min_flux: float
 
     def middle_time(self) -> float:
         """
@@ -31,13 +32,21 @@ class Group:
         return (self.end_time - self.start_time) / 2
 
     @staticmethod
-    def generate_group(datapoints: list[Datapoint]):
+    def generate_group(data: pd.DataFrame):
         """
-        Generates a group from a list of datapoints
+        Generates a group from a list of datapoints sorted by time.
+        There must be at least one item in the datapoints dataframe to avoid an exception.
 
-        :param datapoints: The datapoints to convert into a group
+        :param data: The datapoints to convert into a group. There must be two columns: flux and time.
         :return: A group object that references the datapoints
         """
 
-        raise NotImplementedError()
+        start_time = data.time[0]
+        end_time = data.time[-1]
+
+        mean_flux = data.flux.mean()
+        peak_flux = data.flux.max()
+        min_flux = data.flux.min()
+
+        return Group(start_time, end_time, mean_flux, peak_flux, min_flux)
 
