ffad7d5acdf5fcfc9b6bef6e19789d6dce81144f
AlexanderJCS <alexandercastronovo@gmail.com>
2023-11-30 08:31:11 -0500

    Create datapont dataclass

--- src/exospotter/grouping.py
+++ src/exospotter/grouping.py
@@ -2,14 +2,28 @@ from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
+class Datapoint:
+    """
+    Used to represent a lightcurve datapoint.
+    """
+
+    flux: float
+    time: float
+
+
+@dataclass(frozen=True)
 class Group:
+    """
+    Used to represent a group of datapoints between two time intervals.
+    """
+
     start_time: float
     end_time: float
     mean_flux: float
-    peak_flux: float
-    min_flux: float
+    peak_flux_dp: Datapoint
+    min_flux_dp: Datapoint
 
-    def middle_time(self):
+    def middle_time(self) -> float:
         """
         :return: The midpoint of self.start_time and self.end_time
         """
